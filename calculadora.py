4842097e25f8f7e36c2a6360ba6866774e267c14
Caio <caionovaes3287@gmail.com>
2024-04-30 21:11:21 -0300

    CalculadoraPython

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+num1= int(input("Insira um número"))
+num2= int(input("insira outro número"))
+
+print(" O resultado da soma é", num1 + num2)
+print(" O resultado da subtração é", num1 - num2)
+print(" O resultado da multiplicação é", num1 * num2)
+print(" O resultado da divisão é", num1 / num2)
